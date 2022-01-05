ae28f5273291c5cbb6bafa4553e8ff52814aee1f
saad404 <22587048+saad404@users.noreply.github.com>
2022-01-05 16:01:00 +0100

    cleanup in tests

--- src/test/java/com/example/promotionengine/service/PromotionServiceTest.java
+++ src/test/java/com/example/promotionengine/service/PromotionServiceTest.java
@@ -18,31 +18,62 @@ public class PromotionServiceTest {
     Product prodD = new Product(SKU.D, "Product D is added", BigDecimal.valueOf(15));
 
     @Test
-    public void calculatePromotionWithPromotionAppliedScenarioATest() {
-        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA,
-                BigDecimal.valueOf(130)));
+    public void calculateFinalPriceWithoutPromotionScenarioATest() {
+        /** Scenario A
+         *  1 * A
+         *  1 * B
+         *  1 * C
+         *  =====
+         *  Total 100
+         * */
         List<Product> products = Arrays.asList(prodA, prodB, prodC);
         Cart cart = new Cart(products);
-        assertEquals(new BigDecimal(100), service.calculateFinalCartPrice(cart, promotions));
+        assertEquals(BigDecimal.valueOf(100), service.calculateFinalCartPrice(cart, null));
     }
 
     @Test
-    public void calculateFinalPriceAfterSatisfyingPromotionScenarioBTest() {
-        //5 A's, 5 B's, 1 C
+    public void calculateFinalPriceAfterPromotionScenarioBTest() {
+        /** Scenario B
+         *  5 * A
+         *  5 * B
+         *  1 * C
+         *  =====
+         *  Total 370
+         * */
         List<Product> products = Arrays.asList(prodA, prodA, prodA, prodA, prodA, prodB, prodB, prodB, prodB, prodB,
                 prodC);
         Cart cart = new Cart(products);
-        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA,
-                BigDecimal.valueOf(130)), new Promotion(true, PromotionName.TWOB, BigDecimal.valueOf(45)),
+        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA, BigDecimal.
+                        valueOf(130)), new Promotion(true, PromotionName.TWOB, BigDecimal.valueOf(45)),
                 new Promotion(true, PromotionName.CD, BigDecimal.valueOf(30)));
         assertEquals(BigDecimal.valueOf(370), service.calculateFinalCartPrice(cart, promotions));
     }
 
     @Test
-    public void calculateFinalPriceWithoutPromotionTest() {
+    public void calculateFinalPriceAfterPromotionScenarioCTest() {
+        /** Scenario C
+         *  3 * A
+         *  5 * B
+         *  1 * C
+         *  1 * D
+         *  =====
+         *  Total 280
+         * */
+        List<Product> products = Arrays.asList(prodA, prodA, prodA, prodB, prodB, prodB, prodB, prodB, prodC, prodD);
+        Cart cart = new Cart(products);
+        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA,
+                        BigDecimal.valueOf(130)), new Promotion(true, PromotionName.TWOB, BigDecimal.valueOf(45)),
+                new Promotion(true, PromotionName.CD, BigDecimal.valueOf(30)));
+        assertEquals(BigDecimal.valueOf(280), service.calculateFinalCartPrice(cart, promotions));
+    }
+
+    @Test
+    public void calculatePromotionWithPromotionAppliedTest() {
+        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA,
+                BigDecimal.valueOf(130)));
         List<Product> products = Arrays.asList(prodA, prodB, prodC);
         Cart cart = new Cart(products);
-        assertEquals(BigDecimal.valueOf(100), service.calculateFinalCartPrice(cart, null));
+        assertEquals(new BigDecimal(100), service.calculateFinalCartPrice(cart, promotions));
     }
 
     @Test
@@ -60,24 +91,4 @@ public class PromotionServiceTest {
                 .getPromoPrice());
     }
 
-    @Test
-    public void calculateFinalPriceAfterPromotionScenarioBTest() {
-        List<Product> products = Arrays.asList(prodA, prodA, prodA, prodA, prodA, prodB, prodB, prodB, prodB, prodB,
-                prodC);
-        Cart cart = new Cart(products);
-        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA, BigDecimal.
-                valueOf(130)), new Promotion(true, PromotionName.TWOB, BigDecimal.valueOf(45)),
-                new Promotion(true, PromotionName.CD, BigDecimal.valueOf(30)));
-        assertEquals(BigDecimal.valueOf(370), service.calculateFinalCartPrice(cart, promotions));
-    }
-
-    @Test
-    public void calculateFinalPriceAfterPromotionScenarioCTest() {
-        List<Product> products = Arrays.asList(prodA, prodA, prodA, prodB, prodB, prodB, prodB, prodB, prodC, prodD);
-        Cart cart = new Cart(products);
-        List<Promotion> promotions = Arrays.asList(new Promotion(true, PromotionName.THREEA,
-                BigDecimal.valueOf(130)), new Promotion(true, PromotionName.TWOB, BigDecimal.valueOf(45)),
-                new Promotion(true, PromotionName.CD, BigDecimal.valueOf(30)));
-        assertEquals(BigDecimal.valueOf(280), service.calculateFinalCartPrice(cart, promotions));
-    }
 }
